4197063bd75ce07fae305f725f995480989358e8
chuan <chua_n@yeah.net>
2020-12-07 21:00:57 +0800

    modify typos

--- mayaviOffScreen.py
+++ mayaviOffScreen.py
@@ -9,7 +9,7 @@ Notes:
 -------
     1. If you are using mayavi from remote host by SSH, etc, you must 
         import `mlab` from this module. Or something wrong happens!
-    1. Under the circumstance above, if you need import some other modules meanwhile
+    2. Under the circumstance above, if you need import some other modules meanwhile
         and those modules import `mlab` as well in themselves, make sure the sentence
         `from mayaviOffScreen import mlab` appears before `import otherModule`!
         So that `mayaviOffScreen.mlab` covers `otherModule.mlab`.
