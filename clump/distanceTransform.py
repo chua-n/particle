4fa9a804ac35e9a8bd314a9cf85f3dd4aeb64970
chuan <chua_n@yeah.net>
2021-03-25 22:32:45 +0800

    update my clump generation method

--- clump/distanceTransform.py
+++ clump/distanceTransform.py
@@ -4,11 +4,9 @@
 """
 import numpy as np
 from scipy import ndimage
-from mayavi import mlab
-from particle.pipeline import Sand
 
 
-def run(cube: np.array):
+def oldRun(cube: np.array):
     """Input a 3d image of a particle to get its clump."""
     distImg = ndimage.distance_transform_bf(cube)
     pointCloud = np.array(np.nonzero(distImg)).T
@@ -25,28 +23,63 @@ def run(cube: np.array):
     return centers, radii
 
 
+def intersectionAngle(center1, r1, center2, r2):
+    """cos \phi = (d^2 - r1^2 - r2^2) / 2*r1*r2
+
+    Return:
+    -------
+    phi: 角度，角度制
+    """
+    center1 = np.array(center1)
+    center2 = np.array(center2)
+    dSquare = np.sum((center1 - center2) ** 2)
+    # RuntimeWarning: invalid value encountered in arccos
+    cosPhi = (dSquare - r1**2 - r2**2) / (2*r1*r2)
+    phi = np.arccos(cosPhi)
+    phi *= (180 / np.pi)
+    return phi
+
+
+def run(cube, rho, phi):
+    """Input a 3d image of a particle to get its clump."""
+    distImg = ndimage.distance_transform_edt(cube)
+    allCoords = np.nonzero(distImg)
+    allRadii = distImg[allCoords]
+    allCoords = np.array(allCoords).T
+    # 按半径由大到小排序
+    sortedIdx = np.argsort(allRadii)[::-1]
+    # 待返回的clump球坐标及半径
+    resCoords, resRadii = [], []
+    rMax = allRadii[sortedIdx[0]]
+    for i in sortedIdx:
+        if allRadii[i] / rMax > rho:
+            flag = True
+            for ctr, rds in zip(resCoords, resRadii):
+                angle = intersectionAngle(allCoords[i], allRadii[i], ctr, rds)
+                if angle > phi:
+                    flag = False
+                    break
+            if flag:
+                resCoords.append(allCoords[i])
+                resRadii.append(allRadii[i])
+    resCoords = np.array(resCoords)
+    resRadii = np.array(resRadii)
+    return resCoords, resRadii
+
+
 if __name__ == "__main__":
-    # data = np.load("./data/special/48.npy")
-    # data = skimage.transform.rescale(data, 2)
-    # data = data.reshape(1, 1, *data.shape)
-    data = np.load("data/liutao/v1/particles.npz")["test"]
+    from mayavi import mlab
+    data = np.load("data/liutao/v1/particles.npz")["testSet"]
     print(data.shape)
-    for i in range(len(data)):
+    for i in range(10, len(data)):
         cube = data[i, 0]
-        c, r = run(cube)
+        c, r = run(cube, 0.3, 150)
 
-        # 剔除最小的颗粒
-        mask = r != r.min()
-        c = c[mask]
-        r = r[mask]
+        print(f"Total {len(r)} spheres.")
 
-        mlab.points3d(*c.T, r*2, scale_factor=1,
-                      resolution=30, mode="sphere")
+        mlab.points3d(*c.T, r*2, scale_factor=1, opacity=1,
+                      resolution=30, mode="sphere", color=(0.65,)*3)
+        mlab.title(f"Total {len(r)} spheres.")
         mlab.outline()
         mlab.axes()
-        mlab.title(f"Total {len(r)} spheres.")
-
-        sand = Sand(cube)
-        sand.visualize(vivid=False)
-
         mlab.show()
