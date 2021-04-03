fc3791ab392d037e9f9919d30cd6550a7d92181b
chuan <chua_n@yeah.net>
2021-04-03 11:11:17 +0800

    merge threeViews.py into tvsnet.py

--- nn/tvsnet.py
+++ nn/tvsnet.py
@@ -19,7 +19,106 @@ def getProjections(sourceSet: torch.Tensor):
     return projections
 
 
-class Encoder(nn.Module):
+class TVSDataset(Dataset):
+    def __init__(self, dataset, transform=None) -> None:
+        super().__init__()
+        self.dataset = dataset
+        self.transform = transform
+
+    def __len__(self):
+        return len(self.dataset)
+
+    def __getitem__(self, idx):
+        cubeImage = self.dataset[idx]
+        if self.transform is not None:
+            cubeImage = self.transform(cubeImage)
+        cube = cubeImage[0]
+        projection = torch.empty(3, cube.size(-1), cube.size(-1),
+                                 dtype=cube.dtype, device=cube.device)
+        for i in range(3):
+            projection[i] = project(cube, i)
+        return projection, cubeImage
+
+
+class TVSNet(nn.Module):
+    """Reconstruct a particle from it's three views from x, y, z orientation.
+    """
+
+    def __init__(self, xmlFile):
+        super().__init__()
+        hp, nnParams = parseConfig(xmlFile)
+        self.hp = hp
+        self.convBlock = nn.Sequential()
+        self.fcBlock = nn.Sequential()
+        self.convTransposeBlock = nn.Sequential()
+        self.fcBlockInFeatures = None
+        self.fcBlockOutFeatures = None
+        for layerType, layerParam in nnParams.items():
+            if layerType.startswith("fc-"):
+                if self.fcBlockInFeatures is None:
+                    self.fcBlockInFeatures = nnParams[layerType]["in_features"]
+                self.fcBlockOutFeatures = nnParams[layerType]["out_features"]
+                self.fcBlock.add_module(
+                    layerType.split("-")[-1], constructOneLayer(layerType, layerParam))
+            elif layerType.startswith("convT-"):
+                self.convTransposeBlock.add_module(
+                    layerType.split("-")[-1], constructOneLayer(layerType, layerParam))
+            else:
+                self.convBlock.add_module(
+                    layerType.split("-")[-1], constructOneLayer(layerType, layerParam))
+
+    def forward(self, x):
+        x = self.convBlock(x)
+        x = x.view(x.size(0), self.fcBlockInFeatures)
+        x = self.fcBlock(x)
+        x = x.view(x.size(0), self.fcBlockOutFeatures, 1, 1, 1)
+        x = self.convTransposeBlock(x)
+        return x
+
+    def contrast(self, x, y, voxel=False, glyph='sphere'):
+        """对于输入的数据源颗粒y及其投影x，对比其原始的颗粒图像与对应的重建颗粒的图像，
+        默认绘制的是三维面重建后再采用高斯模糊的图像。
+
+        Parameters:
+        -----------
+        x(array_like): 3 * 64 * 64
+        y(array_like): 64 * 64 * 64
+        voxel(bool, optional): Whether to draw a voxel-represented figure
+        glyph(str, optional): The glyph used to represent a single voxel, works only
+            when `voxel=True`.
+        """
+        with torch.no_grad():
+            x = torch.as_tensor(x, dtype=torch.float32)
+            y = torch.as_tensor(y)
+            x, y = x.view(1, *x.shape), y.view(1, 1, *y.shape)
+            y_re = self.forward(x)
+            raw_cube = Sand(y[0, 0].detach().numpy())
+            fake_cube = Sand(y_re[0, 0].detach().numpy())
+            fig1 = raw_cube.visualize(figure='Original Particle',
+                                      voxel=voxel, glyph=glyph)
+            fig2 = fake_cube.visualize(figure='Reconstructed Particle',
+                                       voxel=voxel, glyph=glyph, scale_mode='scalar')
+        return fig1, fig2
+
+    def generate(self, x, color=(0.65, 0.65, 0.65), opacity=1.0, voxel=False, glyph='sphere'):
+        """
+        Parameters:
+        -----------
+        x(array_like): 3 * 64 * 64
+        voxel(bool, optional): Whether to draw a voxelization figure
+        glyph(str, optinoal): The glyph represents a single voxel, this argument
+            works only when `voxel=True`"""
+        with torch.no_grad():
+            x = torch.as_tensor(x, dtype=torch.float32)
+            x.unsqueeze_(dim=0)
+            y_re = self.forward(x)
+            cube = Sand(y_re[0, 0].detach().numpy())
+            fig = cube.visualize(figure='Generated Particle', color=color, opacity=opacity,
+                                 voxel=voxel, glyph=glyph, scale_mode='scalar')
+        return fig
+
+
+class VaeEncoder(nn.Module):
     def __init__(self, xmlFile):
         super().__init__()
         hp, nnParams = parseConfig(xmlFile)
@@ -46,7 +145,7 @@ class Encoder(nn.Module):
         return mu, logSigma
 
 
-class Decoder(nn.Module):
+class VaeDecoder(nn.Module):
     def __init__(self, xmlFile):
         super().__init__()
         hp, nnParams = parseConfig(xmlFile)
@@ -65,7 +164,7 @@ class Decoder(nn.Module):
         return reconstruction
 
 
-class TVSNet(nn.Module):
+class VaeTVSNet(nn.Module):
     """Three Views Stereo Net. Reconstruct a particle from it's 
     three views from x, y, z orientation.
     """
@@ -75,8 +174,8 @@ class TVSNet(nn.Module):
         super().__init__()
         hp, _ = parseConfig(xmlFile)
         self.lamb = hp['lambda']
-        self.encoder = Encoder(xmlFile)
-        self.decoder = Decoder(xmlFile)
+        self.encoder = VaeEncoder(xmlFile)
+        self.decoder = VaeDecoder(xmlFile)
 
     def forward(self, x):
         mu, logSigma = self.encoder(x)
@@ -150,32 +249,15 @@ class TVSNet(nn.Module):
         return fig
 
 
-class TVSDataset(Dataset):
-    def __init__(self, dataset, transform=None) -> None:
-        super().__init__()
-        self.dataset = dataset
-        self.transform = transform
-
-    def __len__(self):
-        return len(self.dataset)
-
-    def __getitem__(self, idx):
-        cubeImage = self.dataset[idx]
-        if self.transform is not None:
-            cubeImage = self.transform(cubeImage)
-        cube = cubeImage[0]
-        projection = torch.empty(3, cube.size(-1), cube.size(-1),
-                                 dtype=cube.dtype, device=cube.device)
-        for i in range(3):
-            projection[i] = project(cube, i)
-        return projection, cubeImage
-
-
 def train(sourcePath="data/liutao/v1/particles.npz",
           xml="particle/nn/config/tvsnet.xml",
           device=torch.device("cuda" if torch.cuda.is_available() else "cpu"),
           logDir="output/tvsnet",
-          ckptDir="output/tvsnet"):
+          ckptDir="output/tvsnet",
+          kernel=None):
+    # kernel参数指定TVSNet模型的内核采用vae模式还是自己编写的模式
+    assert kernel in ("vae", None)
+
     # build train set & test set
     hp, _ = parseConfig(xml)
     trainSet = loadNnData(sourcePath, 'trainSet')
@@ -186,10 +268,17 @@ def train(sourcePath="data/liutao/v1/particles.npz",
                          batch_size=hp['bs']*2, shuffle=False)
 
     # build and initilize TVSNet model
-    model = TVSNet(xml).to(device)
-    model.initialize()
+    if kernel == "vae":
+        model = VaeTVSNet(xml).to(device)
+        model.initialize()
+        lossFn = model.lossFn
+    else:
+        model = TVSNet(xml).to(device)
+        bceLossFn = nn.BCELoss(reduction="sum")
+        def lossFn(ipt, tgt): return bceLossFn(ipt, tgt)/ipt.size(0)
+
+    # set optimizer
     optim = torch.optim.Adam(model.parameters(), hp['lr'])
-    lossFn = model.lossFn
 
     # set output settings
     logger = setLogger("TVSNet", logDir)
@@ -203,13 +292,23 @@ def train(sourcePath="data/liutao/v1/particles.npz",
         for i, (x, y) in enumerate(trainSet):
             x = x.to(dtype=torch.float32, device=device)
             y = y.to(dtype=torch.float32, device=device)
-            lossRe, lossKL, loss = lossFn(y, *model(x))
-            optim.zero_grad()
-            loss.backward()
-            optim.step()
-            if (i + 1) % 10 == 0 or (i + 1) == len(trainSet):
-                logger.info("[Epoch {}/{}] [Step {}/{}] [lossRe {:.4f}] [lossKL {:.4f}] [loss {:.4f}]".
-                            format(epoch+1, hp["nEpoch"], i+1, len(trainSet), lossRe.item(), lossKL.item(), loss.item()))
+            if kernel == "vae":
+                lossRe, lossKL, loss = lossFn(y, *model(x))
+                optim.zero_grad()
+                loss.backward()
+                optim.step()
+                if (i + 1) % 10 == 0 or (i + 1) == len(trainSet):
+                    logger.info("[Epoch {}/{}] [Step {}/{}] [lossRe {:.4f}] [lossKL {:.4f}] [loss {:.4f}]".
+                                format(epoch+1, hp["nEpoch"], i+1, len(trainSet), lossRe.item(), lossKL.item(), loss.item()))
+            else:
+                yRe = model(x)
+                loss = lossFn(yRe, y)
+                optim.zero_grad()
+                loss.backward()
+                optim.step()
+                if (i + 1) % 10 == 0 or (i + 1) == len(trainSet):
+                    logger.info("[Epoch {}/{}] [Step {}/{}] [lossRe {:.4f}]".
+                                format(epoch+1, model.hp["nEpoch"], i+1, len(trainSet), loss.item()))
 
         # 评估在测试集上的损失
         model.eval()
@@ -218,8 +317,12 @@ def train(sourcePath="data/liutao/v1/particles.npz",
             # 为啥autopep8非要把我的lambda表达式给换成def函数形式......
             def transfer(x): return x.to(dtype=torch.float32, device=device)
             # sum函数将其内部视为生成器表达式？？？
-            testLoss = sum(lossFn(transfer(y), *model(transfer(x)))[-1]
-                           for (x, y) in testSet)
+            if kernel == "vae":
+                testLoss = sum(lossFn(transfer(y), *model(transfer(x)))[-1]
+                               for (x, y) in testSet)
+            else:
+                testLoss = sum(lossFn(model(transfer(x)), transfer(y))
+                               for (x, y) in testSet)
             testLoss /= len(testSet)  # 这里取平均数
         logger.info(f"loss in test set: [testLoss {testLoss:.4f}]")
         # 确认是否保存模型参数
@@ -237,4 +340,5 @@ def train(sourcePath="data/liutao/v1/particles.npz",
 
 if __name__ == "__main__":
     torch.manual_seed(3.14)
-    train()
+    train(kernel=None)
+    # train(kernel="vae", xml="particle/nn/config/tvsnet-vae-deep.xml")
